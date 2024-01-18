f56205401dbbd4101b5e19b1d735cc91b2ba494c
Yash . <thecodeyash@gmail.com>
2024-01-18 21:24:33 +0530

    Project section Added

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ const slider = document.querySelector(".slider");
 
 projectsBtn.addEventListener("click", () => {
   slider.style.left = '6px'; 
-  projectsBtn.classList.add("active");
-  skillsBtn.classList.remove("active");
 });
 
 skillsBtn.addEventListener("click", () => {
   slider.style.left = 'calc(50% + 6px)';
-  skillsBtn.classList.add("active");
-  projectsBtn.classList.remove("active");
 });
