401f304b5270e4fb4ba4383eeda95a8f318a5d32
Yash . <thecodeyash@gmail.com>
2024-01-21 00:28:42 +0530

    Project Skill Slider Code Added

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const projectsBtn = document.querySelector(".Projects");
 const skillsBtn = document.querySelector(".Skills");
 const slider = document.querySelector(".slider");
+const projectSec = document.querySelector(".project-section"); 
+const skillSec = document.querySelector(".skill-section");
 
 projectsBtn.addEventListener("click", () => {
   slider.style.left = '6px'; 
+  projectSec.classList.toggle("hidden");
+  skillSec.classList.toggle("hidden");
 });
 
 skillsBtn.addEventListener("click", () => {
   slider.style.left = 'calc(50% + 6px)';
+  projectSec.classList.toggle("hidden");
+  skillSec.classList.toggle("hidden");
 });
